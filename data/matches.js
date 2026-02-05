a028e64c39c11f3b610b2a35dd3a0d9507010976
Luis Vergara <luis.vergara.catalan@gmail.com>
2026-02-05 19:43:18 -0300

    upload data

---
+++ data/matches.js
@@ -0,0 +1,5 @@
+export const MATCHES = [
+  { id: '1', home: 'Chile', away: 'Perú', resultHome: '2', resultVisit: '4' },
+  { id: '2', home: 'Brasil', away: 'Argentina', resultHome: '3', resultVisit: '2' },
+  { id: '3', home: 'Uruguay', away: 'Colombia', resultHome: '2', resultVisit: '0' },
+];
---
+++ data/ranking.js
@@ -0,0 +1,6 @@
+export const FRIENDS = [
+  { id: '2', name: 'Pepito', puntos: 3 },
+  { id: '1', name: 'Juan', puntos: 5 },
+  { id: '3', name: 'Sebastian', puntos: 6 },
+  { id: '4', name: 'Carlos', puntos: 2 },
+];
